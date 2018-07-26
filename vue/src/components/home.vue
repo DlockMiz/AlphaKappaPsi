46db42b038c8af9cb1ececce11248e0b8df985a2
DlockMiz <daniel.w.lock@gmail.com>
2018-07-26 16:04:19 -0500

    couple home changes

--- vue/src/components/home.vue
+++ vue/src/components/home.vue
@@ -5,15 +5,18 @@
       <textarea v-model="comments" style="width: 100%; height: 300px;"></textarea>
       <button @click="submitComments()">Submit Comments</button>
     </div>
-    <div style="width: 15%; float:left;">
-      <div style="line-height: 40px; float: left; margin-left: 20px; font-size: 15px; width:100%; height: 300px;">If you have any suggestions to make the website flow better or any small quality of life improvments, please leave a comment, anything I can do to improve the site goes a long way for everyone! <strong><br><br>Thanks - Daniel</strong>
+    <div style="width: 50%; float:left;">
+      <div style="line-height: 30px; float: left; margin-left: 20px; font-size: 15px; width:20%; height: 400px;">If you have any suggestions to make the website flow better or any small quality of life improvments, please leave a comment, anything I can do to improve the site goes a long way for everyone! <strong><br><br>Thanks - Daniel</strong>
       </div>
     </div>
-    <div style="float:right;">
-      <div style="float:right;" id="fb-root"></div>
-      <div class="fb-page" data-href="https://www.facebook.com/AKPsiMizzou" data-tabs="timeline" data-width="500" data-height="1000" data-small-header="true" data-adapt-container-width="false" data-hide-cover="true" data-show-facepile="false">
+    <div style="float:right; width: 25%;">
+      <div class="facebook" style="float:right;" id="fb-root"></div>
+      <div class="fb-page facebook" data-href="https://www.facebook.com/AKPsiMizzou" data-tabs="timeline" data-width="500" data-height="1000" data-small-header="true" data-adapt-container-width="false" data-hide-cover="true" data-show-facepile="false">
         <blockquote cite="https://www.facebook.com/AKPsiMizzou" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AKPsiMizzou">Alpha Kappa Psi - Mizzou</a></blockquote>
       </div>
+    </div>
+    <div style="float:left; width: 70%;">
+      <h1><strong>Upcoming Events</strong></h1>
     </div>
   </div>
 </template>
@@ -30,6 +33,7 @@ $(window).on('load', function() {
     js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1';
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
+  $(".facebook").delay(2000).animate({ opacity: 1 }, 2000)  
 })
 
 export default {
@@ -62,6 +66,11 @@ export default {
 <style>
 #homeBody b {
   font-size: 16pt;
+}
+.facebook {
+    opacity: 0;
+    font-size: 21px;
+    text-align: center;
 }
 
 </style>
