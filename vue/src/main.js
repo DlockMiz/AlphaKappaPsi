e9c328500542a4f58cb7766b429439ca6c8d9149
DlockMiz <daniel.w.lock@gmail.com>
2018-09-02 18:57:27 -0500

    forgot an s

--- vue/src/main.js
+++ vue/src/main.js
@@ -39,7 +39,7 @@ xhttp.onreadystatechange = function() {
 if(window.location.hostname == 'localhost')
 	call = 'http://akp.test/api/getGoogleApiKey'
 else
-	call = 'http://server.akpsimiz.com/api/getGoogleApiKey'
+	call = 'https://server.akpsimiz.com/api/getGoogleApiKey'
 
 xhttp.open("POST", call, true);
 xhttp.send();
