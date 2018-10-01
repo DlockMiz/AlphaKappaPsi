f3b3ded2880cef06dab3586648cbfdd2008dbf2a
DlockMiz <daniel.w.lock@gmail.com>
2018-10-01 16:29:35 -0500

    req fix

--- vue/src/components/account_components/requirements_page.vue
+++ vue/src/components/account_components/requirements_page.vue
@@ -20,7 +20,7 @@
           Required Events: {{fund_requirement}}
           <hr> {{fundraising}} - Completed Events
         </div>
-        <div v-if="$store.state.user.id == 1" style="margin-top: 30px;">
+        <div v-if="$store.state.user.status == 1" style="margin-top: 30px;">
           <a style="font-size: .7vw; width: 50%;" @click="changeReqParam('fundraising', fund_requirement)" class="button is-info is-small">Change Required Events</a>
         </div>
       </div>
@@ -30,7 +30,7 @@
           Required Hours: {{service_req}}
           <hr> {{service_hours}} - Completed Hours
         </div>
-        <div v-if="$store.state.user.id == 1" style="margin-top: 30px;">
+        <div v-if="$store.state.user.status == 1" style="margin-top: 30px;">
           <a style="font-size: .7vw; width: 50%;" @click="changeReqParam('service', service_req)" class="button is-info is-small">Change Required Service Hours</a>
         </div>
       </div>
@@ -40,7 +40,7 @@
           Required Events: {{prof_dev_req}}
           <hr> {{prof_dev}} - Completed Events
         </div>
-        <div v-if="$store.state.user.id == 1" style="margin-top: 30px;">
+        <div v-if="$store.state.user.status == 1" style="margin-top: 30px;">
           <a style="font-size: .7vw; width: 50%;" @click="changeReqParam('prof_dev', prof_dev_req)" class="button is-info is-small">Change Prof Dev Requirements</a>
         </div>
       </div>
